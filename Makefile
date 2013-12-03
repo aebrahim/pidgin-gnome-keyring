034e29077f38f826839fe2ebba9e6cea37c6c33d
Ali Ebrahim <ali.ebrahim314@gmail.com>
2013-12-02 22:44:35 -0800

    update build for git

--- Makefile
+++ Makefile
@@ -2,7 +2,11 @@ TARGET = gnome-keyring
 
 KEYRINGFLAGS = `pkg-config --libs --cflags gnome-keyring-1`
 PURPLEFLAGS = `pkg-config --cflags purple`
-VERSION = `git describe`
+VERSION = `git describe --tags`
+ifeq ($(strip $(VERSION)),)
+VERSION = `cat  VERSION`
+echo $(VERSION)
+endif
 
 
 all: ${TARGET}.so
@@ -12,8 +16,14 @@ clean:
 	rm -rf pidgin-${TARGET}-*
 
 ${TARGET}.so: ${TARGET}.c
-	${CC} -Wall -I. -g -O2 ${TARGET}.c -o ${TARGET}.so -shared -fPIC -DPIC -ggdb ${PURPLEFLAGS} ${KEYRINGFLAGS} -DVERSION=${VERSION}
+
+	${CC} -Wall -I. -g -O2 ${TARGET}.c -o ${TARGET}.so -shared -fPIC -DPIC -ggdb ${PURPLEFLAGS} ${KEYRINGFLAGS} -DVERSION=\"${VERSION}\"
 
 install: ${TARGET}.so
+	mkdir -p /usr/lib/purple-2/
+	cp ${TARGET}.so /usr/lib/purple-2/
+
+install_local: ${TARGET}.so
 	mkdir -p ~/.purple/plugins
 	cp ${TARGET}.so ~/.purple/plugins/
+
--- make_package.py
+++ make_package.py
@@ -2,54 +2,44 @@
 """helps automate building the package"""
 import shutil
 import os
-import update_version
+from subprocess import check_output
+from pygit2 import Repository, GIT_SORT_TIME
 import xml.dom.minidom
 import re
 import time
 
 # declare strings up here
-ubuntunames = ["lucid", "natty", "oneiric", "precise"]
-authorname = "Ali Ebrahim"
+ubuntunames = ["lucid", "natty", "oneiric", "precise", "quantal", "raring", "saucy"]
+author_name = "Ali Ebrahim"
+email = "ali.ebrahim314@gmail.com"
 basename = "pidgin-gnome-keyring"
 installdir = "/usr/lib/purple-2/"
 
-# get the svn version
-svn_version = str(update_version.get_svn_version())
-if svn_version[-1].lower() == "m":
-    svn_version = svn_version[0:-1]
-
-# make a blank file
-with open("blank_file", "w") as outfile:
-    outfile.write("\n")
+version = check_output(["git", "describe"]).strip()
 
 for ubuntuname in ubuntunames:
-    version_str = str(update_version.base_version)
-    version_str += "."
-    version_str += svn_version
-    version_str += "~%s" % ubuntuname
+    version_str = version + "~" + ubuntuname
 
     package_version_str = version_str + "-1"
 
     dirname = basename + "-" + version_str
     tarname = basename + "_" + version_str + ".orig"
     debname = dirname + "/debian/"
+    print debname
 
     if os.path.isdir(dirname):
        os.system("rm -rf " + dirname) 
     os.mkdir(dirname)
+    os.mkdir(debname)
     shutil.copy2("gnome-keyring.c", dirname)
     # copy everything in Makefile except for the install part at the end
-    makefile_src = open("Makefile","r")
-    makefile_dest = open(dirname+"/Makefile","w")
-    for line in makefile_src:
-        if re.match("install:", line) is not None:
-            break
-        makefile_dest.write(line)
-    makefile_src.close(); makefile_dest.close()
+    shutil.copy2("Makefile", dirname + "/Makefile")
+    with open(dirname + "/VERSION", "w") as outfile:
+		outfile.write(version)
 
-    shutil.make_archive(tarname, "gztar", root_dir=dirname)
+    #shutil.make_archive(tarname, "gztar", root_dir=dirname)
     # run dh_make
-    os.system("cd %s; dh_make -m -c gpl2 < ../blank_file" %dirname)
+    os.system("cd %s; dh_make -m -c gpl2 --createorig -y -a" %dirname)
     # remove extra files and copy source files
     os.system("cd %s; rm -rf *.ex *.EX README*" %debname)
     shutil.copy2("deb_control", debname+"control")
@@ -58,34 +48,20 @@ for ubuntuname in ubuntunames:
     os.system("echo '%s' > %sdirs" %(installdir, debname))
     os.system("echo 'debian/../gnome-keyring.so %s' > %sinstall" \
             %(installdir, debname))
-
-    # generate the change log in the appropriate format
-    os.system("svn log --xml > log.xml")
-    logfile = open("log.xml", "r")
-    logxml = xml.dom.minidom.parse(logfile)
-    logfile.close()
-    os.system("rm log.xml")
-    changelog = open(debname+"changelog", "w")
-    for i, logentry in enumerate(logxml.firstChild.getElementsByTagName("logentry")):
-        if i > 0:
-            continue
-        print  >> changelog, "%s (%s) %s; urgency=low"\
-                   %(basename, package_version_str, ubuntuname)
-        print >>changelog, "" 
-        msg = logentry.getElementsByTagName("msg")[0].firstChild.nodeValue
-        msg = msg.strip()
-        for line in msg.split("\n"):
-            line = line.strip()
-            if len(line) > 0: print >>changelog, "  " + line
-        print >>changelog, "" 
-        email = logentry.getElementsByTagName("author")[0].firstChild.nodeValue
-        date = logentry.getElementsByTagName("date")[0].firstChild.nodeValue
-        date = date.replace(re.findall("\.[0-9]*Z$", date)[0], "")
-        date = time.strptime(date, "%Y-%m-%dT%H:%M:%S")
-        date= time.strftime("%a, %d %b %Y %X", date)
-        print >>changelog, " -- %s <%s>  %s +0000" %(authorname, email, date)
+    # write the changelog
+    changelog = open(debname + "changelog", "w")
+    repo = Repository(".")
+    for commit in repo.walk(repo.head.target, GIT_SORT_TIME):
+		changelog.write("%s (%s) %s; urgency=low\n\n" % (basename, package_version_str, ubuntuname))
+		for commit_line in commit.message.split("\n"):
+			if len(commit_line) > 0:
+				changelog.write("  " + commit_line + "\n")
+		changelog.write("\n")
+		date = time.strftime("%a, %d %b %Y %X", time.gmtime(commit.commit_time))
+		offset = "%+0.04d" % (commit.commit_time_offset / 60 * 1000)
+		changelog.write(" -- %s <%s>  %s %s\n" % (author_name, email, date, offset))
     changelog.close()
     # call debuild
-    os.system("cd %s; debuild -S" %dirname)
+    os.system("cd %s; debuild -S" % dirname)
 
 
